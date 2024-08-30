2a840059965eaa62108ea05f77f91aa4c28627a7
yike5460 <yike5460@163.com>
2024-08-30 11:00:16 +0000

    docs: tweak on name

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
-          <p>&copy; 2024 Intelli-Ops. All rights reserved.</p>
+          <p>&copy; 2024 AWS Industry Builder. All rights reserved.</p>
           <div>
             <a href="#" className="mr-4">Privacy Policy</a>
             <a href="#">Terms of Service</a>
